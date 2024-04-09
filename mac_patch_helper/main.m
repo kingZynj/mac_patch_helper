2068cf83ccbeeab4817f8c5ba4397e12d1e72b87
artemis <marlkiller@voidm.com>
2024-04-09 09:06:42 +0800

    Initial Commit

---
+++ mac_patch_helper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mac_patch_helper
+//
+//  Created by 马治武 on 2024/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
