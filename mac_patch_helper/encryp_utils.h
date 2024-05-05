86148cbfd5a7fad0d568e3de20520cca1ae0a44c
artemis <marlkiller@voidm.com>
2024-05-05 10:28:28 +0800

    dev

--- mac_patch_helper/encryp_utils.h
+++ mac_patch_helper/encryp_utils.h
@@ -10,12 +10,20 @@
 
 @interface EncryptionUtils : NSObject
 
-+ (NSDictionary *)generateKeyPair;
-+ (NSData *)generateSignatureForData:(NSData *)data privateKey:(NSString *)privateKeyString;
++ (NSString *)generateSurgeDeviceId;
++ (NSString *)calculateMD5:(NSString *) input;
 
-+ (BOOL)verifySignatureWithBase64:(NSString *)policy signature:(NSString *)sign publicKey:(NSString *)publicKeyString;
-+ (BOOL)verifySignatureWithByte:(NSData *)policyData signature:(NSData *)signData publicKey:(NSString *)publicKeyString;
++ (NSDictionary *)generateKeyPair:(bool)is_pkcs8;
++ (NSData *)generateSignatureForData:(NSData *)data privateKey:(NSString *)privateKeyString isPKCS8:(bool)is_pkcs8;
+
++ (BOOL)verifySignatureWithBase64:(NSString *)policy signature:(NSString *)sign publicKey:(NSString *)publicKeyString isPKCS8:(bool)is_pkcs8;
++ (BOOL)verifySignatureWithByte:(NSData *)policyData signature:(NSData *)signData publicKey:(NSString *)publicKeyString isPKCS8:(bool)is_pkcs8;
 
 + (NSString *)convertToPEMFormat:(NSData *)keyData withKeyType:(NSString *)keyType;
+
+
++ (NSData *)cccEncryptData:(NSData *)data withKey:(NSData *)key iv:(NSData *)iv;
++ (NSData *)cccDecryptData:(NSData *)data withKey:(NSData *)key iv:(NSData *)iv;
+
 @end
 #endif /* encryp_utils_h */
